1eb2d890c90abd8d0faaa264d9e3cc06a1e7a114
andystrobel <andreas.strobel@googlemail.com>
2015-03-24 15:27:51 +0100

    Fixed CSS classes

--- WikiModuleEvents.php
+++ WikiModuleEvents.php
@@ -31,6 +31,7 @@ class WikiModuleEvents
         if ($event->sender->space->isModuleEnabled('wiki')) {
             $event->sender->addItem(array(
                 'label' => Yii::t('WikiModule.base', 'Wiki'),
+                'group' => 'modules',
                 'url' => $event->sender->space->createUrl('//wiki/page'),
                 'icon' => '<i class="fa fa-book"></i>',
                 'isActive' => (Yii::app()->controller->module && Yii::app()->controller->module->id == 'wiki'),
@@ -45,6 +46,7 @@ class WikiModuleEvents
         if ($user->isModuleEnabled('wiki')) {
             $event->sender->addItem(array(
                 'label' => Yii::t('WikiModule.base', 'Wiki'),
+                'group' => 'modules',
                 'url' => $user->createUrl('//wiki/page'),
                 'isActive' => (Yii::app()->controller->module && Yii::app()->controller->module->id == 'wiki'),
             ));
