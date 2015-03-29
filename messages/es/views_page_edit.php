f0d83db6ac747d537132d31cb0c5b66e0caad77a
Lucas Bartholemy <lucas@bartholemy.com>
2015-03-29 20:56:39 +0200

    Added translation files

---
+++ messages/es/views_page_edit.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  '<strong>Create</strong> new page' => '',
+  '<strong>Edit</strong> page' => '',
+  'Enter a wiki page name or url (e.g. http://example.com)' => '',
+  'New page title' => '',
+  'Page content' => '',
+  'Save' => 'Guardar',
+);
