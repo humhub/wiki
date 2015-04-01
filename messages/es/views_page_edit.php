90521261341b2f47644c2ca202f3d4af2e7f8ab0
Lucas Bartholemy <lucas@bartholemy.com>
2015-04-01 23:58:15 +0200

    Updated translations (translate.humhub.org)

--- messages/es/views_page_edit.php
+++ messages/es/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
+  '<strong>Create</strong> new page' => '<strong>Crear</strong> nueva página',
+  '<strong>Edit</strong> page' => '<strong>Editar</strong> página',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Escribe el nombre de la wiki o la dirección (ej. http://ejemplo.com)',
+  'New page title' => 'Nuevo título de la página',
+  'Page content' => 'Contenido de la página',
   'Save' => 'Guardar',
 );
--- messages/tr/views_page_edit.php
+++ messages/tr/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
+  '<strong>Create</strong> new page' => '<strong>Yeni</strong> sayfa oluştur',
+  '<strong>Edit</strong> page' => '<strong>Sayfayı</strong> Düzenle',
   'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
+  'New page title' => 'Yeni Sayfa Başlığı',
   'Page content' => '',
   'Save' => 'Kaydet',
 );
