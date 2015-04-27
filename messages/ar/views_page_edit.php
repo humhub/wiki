116e1da9fbde202f82607e9fe38907889f14a718
Lucas Bartholemy <lucas@bartholemy.com>
2015-04-27 15:59:52 +0200

    Updated translations (translate.humhub.org)

--- messages/ar/views_page_edit.php
+++ messages/ar/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
-  'Save' => '',
+  '<strong>Create</strong> new page' => '<strong>بدء</strong> صفحة جديدة',
+  '<strong>Edit</strong> page' => '<strong>تعديل</strong> صفحة',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'الرجاء إدخال عنوان صفحة ويكي او الوصلة المؤدية لها مثلاُ http://example.com',
+  'New page title' => 'عنوان صفحة جديد',
+  'Page content' => 'محتويات الصفحة',
+  'Save' => 'حفظ',
 );
