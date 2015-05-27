b2fd2690d0add527c73cbdece2316c7f8257e03f
Lucas Bartholemy <lucas@bartholemy.com>
2015-05-27 19:04:11 +0200

    Check for space object

--- WikiModuleEvents.php
+++ WikiModuleEvents.php
@@ -28,7 +28,7 @@ class WikiModuleEvents
 
     public static function onSpaceMenuInit(CEvent $event)
     {
-        if ($event->sender->space->isModuleEnabled('wiki') && $event->sender->space->isMember()) {
+        if ($event->sender->space !== null && $event->sender->space->isModuleEnabled('wiki') && $event->sender->space->isMember()) {
             $event->sender->addItem(array(
                 'label' => Yii::t('WikiModule.base', 'Wiki'),
                 'group' => 'modules',
