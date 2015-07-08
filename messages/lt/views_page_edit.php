de45ff014dbb938bca66b0a9f9dd3a801e704412
Lucas Bartholemy <lucas@bartholemy.com>
2015-07-08 22:22:18 +0200

    Updated translations (translate.humhub.org)

--- messages/lt/views_page_edit.php
+++ messages/lt/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
-  'Save' => '',
+  '<strong>Create</strong> new page' => '<strong>Sukurti</strong> naują puslapį',
+  '<strong>Edit</strong> page' => '<strong>Taisyti</strong> puslapį',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Įrašykite Wiki puslapį arba adresą (pvz. http://example.com)',
+  'New page title' => 'Naujo puslapio pavadinimas',
+  'Page content' => 'Puslapio turinys',
+  'Save' => 'Išsaugoti',
 );
