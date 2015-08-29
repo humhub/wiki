69621b748936da3dc33704cc35686c18876c9c59
Lucas Bartholemy <lucas@bartholemy.com>
2015-08-29 18:03:21 +0200

    Updated translations (translate.humhub.org)

--- messages/de/views_page_edit.php
+++ messages/de/views_page_edit.php
@@ -3,7 +3,7 @@ return array (
   '<strong>Create</strong> new page' => '',
   '<strong>Edit</strong> page' => '',
   'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
+  'New page title' => 'Neuer Seitentitel',
+  'Page content' => 'Inhalt',
   'Save' => 'Speichern',
 );
