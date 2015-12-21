7b4ac69837b1dc556e1a6882001f291b7ff26f11
Lucas Bartholemy <lucas@bartholemy.com>
2015-12-21 19:01:45 +0100

    Enh: Handle file urls

--- Markdown.php
+++ Markdown.php
@@ -17,10 +17,17 @@ class Markdown extends \humhub\libs\Markdown
     {
 
         if (Yii::$app->controller instanceof ContentContainerController) {
+
+            if (substr($url, 0, 7) == 'file://') {
+                return $url;
+            }
+
             if (substr($url, 0, 10) !== "file-guid-" && substr($url, 0, 1) !== "." && substr($url, 0, 1) !== "/" && substr($url, 0, 7) !== "http://" && substr($url, 0, 8) !== "https://") {
                 return Yii::$app->controller->contentContainer->createUrl('/wiki/page/view', array('title' => $url));
             }
         }
+
+
 
         return parent::handleInternalUrls($url);
     }
