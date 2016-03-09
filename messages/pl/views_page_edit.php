1749cb494e5dfe7daa1677c32af710d49822d5e8
HumHub Translate <info@humhub.com>
2016-03-09 14:54:27 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/views_page_edit.php
+++ messages/pl/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
+  '<strong>Create</strong> new page' => '<strong>Utwórz</strong> nową stronę',
+  '<strong>Edit</strong> page' => '<strong>Edytuj</strong> stronę',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Wprowadź tytuł wiki lub jej url (np. http://przykład.pl)',
+  'New page title' => 'Nowy tytuł strony',
+  'Page content' => 'Zawartość strony',
   'Save' => 'Zapisz ',
 );
