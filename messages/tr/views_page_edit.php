234d0e8355336b4a7af74bda2c3d1d28e5761cb2
HumHub Translate <info@humhub.com>
2016-05-24 15:25:00 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/tr/views_page_edit.php
+++ messages/tr/views_page_edit.php
@@ -2,8 +2,8 @@
 return array (
   '<strong>Create</strong> new page' => '<strong>Yeni</strong> sayfa oluştur',
   '<strong>Edit</strong> page' => '<strong>Sayfayı</strong> Düzenle',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Wiki sayfası adı veya url girin (örnek: http://example.com)',
   'New page title' => 'Yeni Sayfa Başlığı',
-  'Page content' => '',
+  'Page content' => 'Sayfa içeriği',
   'Save' => 'Kaydet',
 );
