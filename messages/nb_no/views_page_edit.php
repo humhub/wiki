78dd0a985f68d9c45ca9e18849f71843f6715ada
HumHub Translations <info@humhub.com>
2017-04-05 09:40:43 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nb_no/views_page_edit.php
+++ messages/nb_no/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
+  '<strong>Create</strong> new page' => '<strong>Opprett</strong> side',
   '<strong>Edit</strong> page' => '<strong>Rediger</strong> side',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Sett inn wiki sidetittel eller url (f.eks. http://example.com)',
+  'New page title' => 'Ny sidetittel',
+  'Page content' => 'Sideinnhold',
   'Save' => 'Lagre',
 );
