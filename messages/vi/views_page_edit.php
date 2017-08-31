89872f60efc61876b21117ba5d2d76dac213e7f7
HumHub Translations <info@humhub.com>
2017-08-31 08:56:00 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/vi/views_page_edit.php
+++ messages/vi/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
-  'Save' => '',
+  '<strong>Create</strong> new page' => '<strong>Tạo</strong> trang mới',
+  '<strong>Edit</strong> page' => '<strong>Chỉnh sửa</strong> trang',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Nhập tên hoặc địa chỉ url của trang wiki (VD: http://vidu.com)',
+  'New page title' => 'Tiêu đề trang mới',
+  'Page content' => 'Nội dung trang',
+  'Save' => 'Lưu',
 );
