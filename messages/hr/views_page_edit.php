f6f6eab47cff17fbbebced0f71ce5f1f940faab0
HumHub Translations <info@humhub.com>
2017-12-20 11:04:31 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hr/views_page_edit.php
+++ messages/hr/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '<strong>Create</strong> new page' => '',
-  '<strong>Edit</strong> page' => '',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
+  '<strong>Create</strong> new page' => '<strong>Kreiraj</strong> novu stranicu',
+  '<strong>Edit</strong> page' => '<strong>Uredi</strong> stranicu',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Unesite naziv wiki stranice ili url (e.g. http://example.com)',
+  'New page title' => 'Novi naziv stranice',
+  'Page content' => 'Sadržaj stranice',
   'Save' => 'Spremi',
 );
