ab24513f285c2ef445cd3b7322a05108b712518a
HumHub Translations <info@humhub.com>
2018-05-08 14:52:13 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/sl/views_page_edit.php
+++ messages/sl/views_page_edit.php
@@ -21,6 +21,5 @@ return [
     '<strong>Edit</strong> page' => '',
     'Enter a wiki page name or url (e.g. http://example.com)' => '',
     'New page title' => '',
-    'Page content' => '',
     'Save' => '',
 ];
