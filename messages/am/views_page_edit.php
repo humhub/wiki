03590459b8fabf28d86fed0142a3e16e80b616f0
HumHub Translations <info@humhub.com>
2018-08-15 11:54:16 +0200

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/am/views_page_edit.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '<strong>Create</strong> new page' => '',
+    '<strong>Edit</strong> page' => '',
+    'Enter a wiki page name or url (e.g. http://example.com)' => '',
+    'New page title' => '',
+    'Save' => '',
+];
