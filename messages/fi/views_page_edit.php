bd5048c66311ed42c56aaa341f8379c06298ea10
HumHub Translations <info@humhub.com>
2018-08-15 11:52:00 +0200

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/fi/views_page_edit.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  '<strong>Create</strong> new page' => '<strong>Luo</strong> uusi sivu',
+  '<strong>Edit</strong> page' => '<strong>Muokkaa</strong> sivua',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Syötä sivun nimi tai url (esim. http://esimerkki.fi)',
+  'New page title' => 'Uusi sivun nimi',
+  'Page content' => 'Sivun sisältö',
+  'Save' => 'Tallenna',
+);
