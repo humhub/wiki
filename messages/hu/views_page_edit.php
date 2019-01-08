38eb2b6ce278546360e7e817aa6d184cbef128a8
HumHub Translations <info@humhub.com>
2019-01-08 11:47:37 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/views_page_edit.php
+++ messages/hu/views_page_edit.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    '<strong>Create</strong> new page' => '<strong>Új oldal</strong> létrehozása',
-    '<strong>Edit</strong> page' => 'Oldal <strong>szerkesztése</strong>',
-    'New page title' => 'Oldal címe',
-];
+return array (
+  '<strong>Create</strong> new page' => '<strong>Új oldal</strong> létrehozása',
+  '<strong>Edit</strong> page' => 'Oldal <strong>szerkesztése</strong>',
+  'New page title' => 'Oldal új címe',
+);
