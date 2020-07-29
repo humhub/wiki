cc18b922f3abd9e68d348f9f579fd64a9b6474d7
buddh4 <mail@jharrer.de>
2020-07-29 11:53:27 +0200

    Update HumHub min version to v 1.4.4

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -4,13 +4,6 @@
 set -ev
 
 # Install chomedriver
-curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip \
+curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/84.0.4147.30/chromedriver_linux64.zip \
     && unzip -o -d $HOME chromedriver_linux64.zip \
 	&& chmod +x $HOME/chromedriver
-
-# Install composer package
-composer global require fxp/composer-asset-plugin
-
-# Install npm
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
