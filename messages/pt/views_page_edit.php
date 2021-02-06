e84032efe95e9c9a02fa3ec8fb318bdb88f5a795
HumHub Translations <info@humhub.com>
2021-02-06 18:32:02 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/views_page_edit.php
+++ messages/pt/views_page_edit.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    '<strong>Create</strong> new page' => '',
-    '<strong>Edit</strong> page' => '',
-    'New page title' => '',
-];
+return array (
+  '<strong>Create</strong> new page' => '<strong>Criar</strong> nova página',
+  '<strong>Edit</strong> page' => '<strong>Editar</strong> página',
+  'New page title' => 'Título da nova página',
+);
