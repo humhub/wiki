4589d016f6cb440d29b9f0144ad427581aaa783d
HumHub Translations <info@humhub.com>
2021-04-11 14:35:00 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/config.php
+++ messages/sv/config.php
@@ -1,6 +1,6 @@
 <?php
 return array (
   'Default wiki settings' => 'Standardinställningar för wiki',
-  'Here you can configure default settings the wiki module.' => '',
-  'Module name' => '',
+  'Here you can configure default settings the wiki module.' => 'Här kan du konfigurera de förvalda inställningarna för wikimodulen.',
+  'Module name' => 'Modulnamn',
 );
