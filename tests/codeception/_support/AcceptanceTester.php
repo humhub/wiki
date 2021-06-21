b9285548f0d6570ec023d0ae03fb45c4b0093f63
Yura Bakhtin <yurybakh@gmail.com>
2021-06-21 13:08:08 +0300

    Test visibility of wiki pages in container menus

--- tests/codeception/_support/AcceptanceTester.php
+++ tests/codeception/_support/AcceptanceTester.php
@@ -8,6 +8,8 @@
 
 namespace wiki;
 
+use Yii;
+
 /**
  * Inherited Methods
  * @method void wantToTest($text)
@@ -30,4 +32,248 @@ class AcceptanceTester extends \AcceptanceTester
    /**
     * Define custom actions here
     */
+
+    public function enableWikiOnProfile()
+    {
+        $this->amOnRoute(['/user/account/edit-modules']);
+        $this->waitForText('Enable');
+
+        if(version_compare(Yii::$app->version  ,'1.4-dev', '<')) {
+            // Note: this only works if no other profile module is installed
+            $this->click('Enable');
+        } else {
+            $this->click('.enable-module-wiki');
+        }
+
+        $this->waitForText('Disable');
+    }
+
+    /**
+     * Create wiki entries
+     *
+     * @throws \Exception
+     */
+    public function createWikiEntries()
+    {
+        /**
+         * CREATE CATEGORY
+         */
+        $this->amGoingTo('Create my first wiki category page');
+        $this->waitForText('Create new page', 30);
+        $this->fillField('#wikipage-title', 'First Test Wiki Category');
+        $this->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My First Wiki Category!');
+        $this->jsShow('.form-collapsible-fields.closed fieldset');
+        $this->click('[for="wikipage-is_category"]');
+        $this->click('Save');
+
+        $this->waitForElementVisible('.wiki-page-content');
+        $this->see('First Test Wiki Category');
+        $this->wait(1);
+        $this->see('My First Wiki Category!', 'h1');
+
+        $this->toIndex();
+        $this->see('First Test Wiki Category', '.wiki-page-list');
+
+        /**
+         * CREATE First SUB PAGE
+         */
+        $this->amGoingTo('Create my first sub page');
+        $this->jsClick('[data-original-title="Add Page"]');
+        $this->waitForText('Create new page', 30);
+
+        $this->fillField('#wikipage-title', 'First Sub Page');
+        $this->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My Sub Page!');
+        $this->jsShow('.form-collapsible-fields.closed fieldset');
+        $this->seeOptionIsSelected('#wikipage-parent_page_id', 'First Test Wiki Category');
+        $this->click('Save');
+
+        $this->waitForElementVisible('.wiki-page-content');
+        $this->see('First Sub Page');
+        $this->see('My Sub Page!', 'h1');
+
+        $this->toIndex();
+        $this->see('First Sub Page', '.wiki-page-list');
+
+        /**
+         * CREATE Second SUB PAGE
+         */
+        $this->amGoingTo('Create my second sub page');
+        $this->jsClick('[data-original-title="Add Page"]');
+        $this->waitForText('Create new page', 30);
+
+        $this->fillField('#wikipage-title', 'Second Page');
+        $this->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My Second Page!');
+        $this->jsShow('.form-collapsible-fields.closed fieldset');
+        $this->seeOptionIsSelected('#wikipage-parent_page_id', 'First Test Wiki Category');
+        $this->click('Save');
+
+        $this->waitForElementVisible('.wiki-page-content');
+        $this->toIndex();
+        $this->see('Second Page', '.wiki-page-list');
+
+        /**
+         * Set First As HomePage
+         */
+        $this->click('First Sub Page');
+
+        $this->waitForText('My Sub Page', null,'.wiki-content');
+        $this->click('Edit page');
+        $this->waitForElementVisible('#wiki-page-edit');
+        $this->jsShow('.form-collapsible-fields.closed fieldset');
+        $this->click('[for="wikipage-is_home"]');
+        $this->click('Save');
+
+        $this->waitForText('Home', null, '.wiki-menu');
+        $this->toIndex();
+
+        $this->click('Wiki', '.layout-nav-container');
+
+        $this->waitForText('My Sub Page', null,'.wiki-content');
+
+        /**
+         * Move Category
+         * skipped due to travis search index issues
+        $this->enableModule(3, 'wiki');
+        $this->amOnSpace(1);
+        //$this->wait(30);
+        $this->waitForText('Wiki', null,'.layout-nav-container');
+        $this->click('Wiki', '.layout-nav-container');
+
+
+        $this->waitForElementVisible('#wiki_index');
+        $this->toIndex();
+        $this->click('First Test Wiki Category');
+        $this->waitForText('My First Wiki Category');
+        $this->click('Edit page');
+
+        $this->waitForElementVisible('#wiki-page-edit');
+        $this->click('Move content');
+
+        $this->waitForText('Move content', null, '#globalModal');
+        $this->selectUserFromPicker('#movecontentform-target', 'Space 3');
+
+        $this->click('Save', '#globalModal');
+        $this->waitForText('Index', null, '.wiki-content');
+        $this->dontSee('First Test Wiki Category');
+        $this->see('First Sub Page', '.wiki-page-list');
+        $this->see('Second Page', '.wiki-page-list');
+        $this->wait(1);
+        $this->click('First Sub Page', '.wiki-page-list');
+        $this->waitForText('My Sub Page');
+         */
+
+        $this->click('Edit page');
+        $this->waitForElementVisible('#wiki-page-edit');
+        $this->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My Sub Page Updated!');
+        $this->click('Save');
+
+        $this->waitForText('Page History');
+
+        $this->see('My Sub Page Updated');
+
+        $this->click('Page History');
+
+        $this->waitForText('Page history', null, '.wiki-content');
+
+        $this->click('View', '.wiki-page-history .media:not(.alert)');
+
+        $this->waitForText('Revert this');
+        $this->click('Revert this');
+
+        $this->waitForText('Confirm page reverting', null, '#globalModalConfirm');
+        $this->click('Revert', '#globalModalConfirm');
+
+        $this->waitForText('My Sub Page!');
+    }
+
+    /**
+     * Create wiki pages
+     *
+     * @param string $type
+     * @throws \Exception
+     */
+    public function createWikiPages(string $type)
+    {
+        /**
+         * CREATE PUBLIC PAGE
+         */
+        $this->amGoingTo("Create my public {$type} wiki page");
+        $this->waitForText('Create new page', 30);
+        $this->fillField('#wikipage-title', "First Public {$type} Wiki Page");
+        $this->fillField('#wikipagerevision-content .humhub-ui-richtext', "# My First Wiki {$type} Public Page!");
+        $this->jsShow('.form-collapsible-fields.closed fieldset');
+        $this->click('[for="pageeditform-ispublic"]');
+        $this->click('Save');
+
+        $this->waitForElementVisible('.wiki-page-content');
+        $this->see("First Public {$type} Wiki Page");
+        $this->wait(1);
+        $this->see("My First Wiki {$type} Public Page!", 'h1');
+
+        $this->seeElement('.fa-globe');
+        $this->toIndex();
+        $this->see("First Public {$type} Wiki Page", '.wiki-page-list');
+
+        /**
+         * CREATE PRIVATE PAGE
+         */
+        $this->amGoingTo("Create my private {$type} wiki page");
+        $this->click('New page');
+        $this->waitForText('Create new page', 30);
+        $this->fillField('#wikipage-title', "First Private {$type} Wiki Page");
+        $this->fillField('#wikipagerevision-content .humhub-ui-richtext', "# My First Wiki {$type} Private Page!");
+        $this->click('Save');
+
+        $this->waitForElementVisible('.wiki-page-content');
+        $this->see("First Private {$type} Wiki Page");
+        $this->wait(1);
+        $this->see("My First Wiki {$type} Private Page!", 'h1');
+        $this->dontSee("Public", 'h1 .label-info');
+        $this->toIndex();
+        $this->see("First Public {$type} Wiki Page", '.wiki-page-list');
+        $this->see("First Private {$type} Wiki Page", '.wiki-page-list');
+    }
+
+    /**
+     * Go to index of wiki pages
+     *
+     * @throws \Exception
+     */
+    public function toIndex()
+    {
+        $this->click('#wiki_index');
+        $this->waitForText('Index', null, '.wiki-content');
+    }
+
+    /**
+     * Show a wiki page in Container menu
+     *
+     * @param string $wikiPageTitle
+     * @param string $sidebarSelector
+     * @throws \Exception
+     */
+    public function showWikiPageInContainerMenu(string $wikiPageTitle, string $sidebarSelector)
+    {
+        $this->click('Wiki', '.layout-nav-container');
+        $this->waitForText('Index', 10, '.wiki-page-content');
+        $this->dontSee($wikiPageTitle, $sidebarSelector);
+
+        $this->waitForText($wikiPageTitle);
+        $this->click($wikiPageTitle);
+        $this->waitForText('Edit page');
+        $this->click('Edit page');
+        $this->waitForText('Advanced settings');
+        $this->jsShow('.form-collapsible-fields.closed fieldset');
+        $this->click('[for="wikipage-is_container_menu"]');
+        $this->fillField('#wikipage-container_menu_order', 100);
+        $this->click('Save');
+
+        $this->waitForText($wikiPageTitle, 10, $sidebarSelector);
+    }
+
+    // Create Category
+    // Add Page
+    // Move Page
+    // Set as homepage
+    // Edit and revert
 }
--- tests/codeception/acceptance/WikiCest.php
+++ tests/codeception/acceptance/WikiCest.php
@@ -9,7 +9,6 @@
 namespace wiki\acceptance;
 
 use wiki\AcceptanceTester;
-use Yii;
 
 class WikiCest
 {
@@ -26,7 +25,7 @@ class WikiCest
         $I->waitForText('No pages created yet.', 15);
         $I->click('Let\'s go!');
 
-        $this->createWikiEntries($I);
+        $I->createWikiEntries();
     }
 
     /**
@@ -37,7 +36,7 @@ class WikiCest
     {
         $I->amUser1();
 
-        $this->enableWikiOnProfile($I);
+        $I->enableWikiOnProfile();
 
         $I->amOnUser1Profile();
 
@@ -45,22 +44,7 @@ class WikiCest
         $I->waitForText('No pages created yet.', 15);
         $I->click('Let\'s go!');
 
-        $this->createWikiEntries($I);
-    }
-
-    private function enableWikiOnProfile(AcceptanceTester $I)
-    {
-        $I->amOnRoute(['/user/account/edit-modules']);
-        $I->waitForText('Enable');
-
-        if(version_compare( Yii::$app->version  ,'1.4-dev', '<')) {
-            // Note: this only works if no other profile module is installed
-            $I->click('Enable');
-        } else {
-            $I->click('.enable-module-wiki');
-        }
-
-        $I->waitForText('Disable');
+        $I->createWikiEntries();
     }
 
     /**
@@ -80,14 +64,14 @@ class WikiCest
         $I->click('Save');
         $I->wait(1);
 
-        $this->enableWikiOnProfile($I);
+        $I->enableWikiOnProfile();
 
         $I->amOnUser1Profile();
         $I->click('Wiki', '.layout-nav-container');
         $I->waitForText('No pages created yet.', 15);
         $I->click('Let\'s go!');
 
-        $this->createWikiPages($I, 'Profile');
+        $I->createWikiPages('Profile');
 
         $I->logout();
 
@@ -113,7 +97,7 @@ class WikiCest
         $I->waitForText('No pages created yet.', 15);
         $I->click('Let\'s go!');
 
-        $this->createWikiPages($I, 'Space');
+        $I->createWikiPages('Space');
 
         $I->logout();
 
@@ -124,207 +108,37 @@ class WikiCest
         $I->dontSee('First Private Space Wiki Page', '.wiki-page-list');
     }
 
-    /**
-     * @param AcceptanceTester $I
-     * @throws \Exception
-     */
-    private function createWikiEntries($I)
+    public function testWikiPageInSpaceMenu(AcceptanceTester $I)
     {
-        /**
-         * CREATE CATEGORY
-         */
-        $I->amGoingTo('Create my first wiki category page');
-        $I->waitForText('Create new page', 30);
-        $I->fillField('#wikipage-title', 'First Test Wiki Category');
-        $I->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My First Wiki Category!');
-        $I->jsShow('.form-collapsible-fields.closed fieldset');
-        $I->click('[for="wikipage-is_category"]');
-        $I->click('Save');
+        $I->amAdmin();
 
-        $I->waitForElementVisible('.wiki-page-content');
-        $I->see('First Test Wiki Category');
-        $I->wait(1);
-        $I->see('My First Wiki Category!', 'h1');
+        $I->amUser1(true);
+        $I->enableModule(2, 'wiki');
+        $I->click('Wiki', '.layout-nav-container');
+        $I->waitForText('No pages created yet.', 15);
+        $I->click('Let\'s go!');
 
-        $this->toIndex($I);
-        $I->see('First Test Wiki Category', '.wiki-page-list');
+        $I->createWikiPages('Space');
 
+        $I->showWikiPageInContainerMenu('First Public Space Wiki Page', '#space-main-menu');
+        $I->showWikiPageInContainerMenu('First Private Space Wiki Page', '#space-main-menu');
+    }
 
-        /**
-         * CREATE First SUB PAGE
-         */
-        $I->amGoingTo('Create my first sub page');
-        $I->jsClick('[data-original-title="Add Page"]');
-        $I->waitForText('Create new page', 30);
+    public function testWikiPageInProfileMenu(AcceptanceTester $I)
+    {
+        $I->amUser1();
 
-        $I->fillField('#wikipage-title', 'First Sub Page');
-        $I->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My Sub Page!');
-        $I->jsShow('.form-collapsible-fields.closed fieldset');
-        $I->seeOptionIsSelected('#wikipage-parent_page_id', 'First Test Wiki Category');
-        $I->click('Save');
+        $I->enableWikiOnProfile();
 
-        $I->waitForElementVisible('.wiki-page-content');
-        $I->see('First Sub Page');
-        $I->see('My Sub Page!', 'h1');
-
-        $this->toIndex($I);
-        $I->see('First Sub Page', '.wiki-page-list');
-
-
-        /**
-         * CREATE Second SUB PAGE
-         */
-
-        $I->amGoingTo('Create my second sub page');
-        $I->jsClick('[data-original-title="Add Page"]');
-        $I->waitForText('Create new page', 30);
-
-        $I->fillField('#wikipage-title', 'Second Page');
-        $I->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My Second Page!');
-        $I->jsShow('.form-collapsible-fields.closed fieldset');
-        $I->seeOptionIsSelected('#wikipage-parent_page_id', 'First Test Wiki Category');
-        $I->click('Save');
-
-        $I->waitForElementVisible('.wiki-page-content');
-        $this->toIndex($I);
-        $I->see('Second Page', '.wiki-page-list');
-
-        /**
-         * Set First As HomePage
-         */
-        $I->click('First Sub Page');
-
-        $I->waitForText('My Sub Page', null,'.wiki-content');
-        $I->click('Edit page');
-        $I->waitForElementVisible('#wiki-page-edit');
-        $I->jsShow('.form-collapsible-fields.closed fieldset');
-        $I->click('[for="wikipage-is_home"]');
-        $I->click('Save');
-
-        $I->waitForText('Home', null, '.wiki-menu');
-        $this->toIndex($I);
+        $I->amOnUser1Profile();
 
         $I->click('Wiki', '.layout-nav-container');
+        $I->waitForText('No pages created yet.', 15);
+        $I->click('Let\'s go!');
 
-        $I->waitForText('My Sub Page', null,'.wiki-content');
+        $I->createWikiPages('Profile');
 
-        /**
-         * Move Category
-         * skipped due to travis search index issues
-        $I->enableModule(3, 'wiki');
-        $I->amOnSpace(1);
-        //$I->wait(30);
-        $I->waitForText('Wiki', null,'.layout-nav-container');
-        $I->click('Wiki', '.layout-nav-container');
-
-
-        $I->waitForElementVisible('#wiki_index');
-        $this->toIndex($I);
-        $I->click('First Test Wiki Category');
-        $I->waitForText('My First Wiki Category');
-        $I->click('Edit page');
-
-        $I->waitForElementVisible('#wiki-page-edit');
-        $I->click('Move content');
-
-        $I->waitForText('Move content', null, '#globalModal');
-        $I->selectUserFromPicker('#movecontentform-target', 'Space 3');
-
-        $I->click('Save', '#globalModal');
-        $I->waitForText('Index', null, '.wiki-content');
-        $I->dontSee('First Test Wiki Category');
-        $I->see('First Sub Page', '.wiki-page-list');
-        $I->see('Second Page', '.wiki-page-list');
-        $I->wait(1);
-        $I->click('First Sub Page', '.wiki-page-list');
-        $I->waitForText('My Sub Page');
-        */
-
-
-        $I->click('Edit page');
-        $I->waitForElementVisible('#wiki-page-edit');
-        $I->fillField('#wikipagerevision-content .humhub-ui-richtext', '# My Sub Page Updated!');
-        $I->click('Save');
-
-        $I->waitForText('Page History');
-
-        $I->see('My Sub Page Updated');
-
-        $I->click('Page History');
-
-        $I->waitForText('Page history', null, '.wiki-content');
-
-        $I->click('View', '.wiki-page-history .media:not(.alert)');
-
-        $I->waitForText('Revert this');
-        $I->click('Revert this');
-
-        $I->waitForText('Confirm page reverting', null, '#globalModalConfirm');
-        $I->click('Revert', '#globalModalConfirm');
-
-        $I->waitForText('My Sub Page!');
+        $I->showWikiPageInContainerMenu('First Public Profile Wiki Page', '.profile-content .left-navigation');
+        $I->showWikiPageInContainerMenu('First Private Profile Wiki Page', '.profile-content .left-navigation');
     }
-
-    /**
-     * @param AcceptanceTester $I
-     * @param $type
-     * @throws \Exception
-     */
-    private function createWikiPages($I, $type)
-    {
-        /**
-         * CREATE PUBLIC PAGE
-         */
-        $I->amGoingTo("Create my public {$type} wiki page");
-        $I->waitForText('Create new page', 30);
-        $I->fillField('#wikipage-title', "First Public {$type} Wiki Page");
-        $I->fillField('#wikipagerevision-content .humhub-ui-richtext', "# My First Wiki {$type} Public Page!");
-        $I->jsShow('.form-collapsible-fields.closed fieldset');
-        $I->click('[for="pageeditform-ispublic"]');
-        $I->click('Save');
-
-        $I->waitForElementVisible('.wiki-page-content');
-        $I->see("First Public {$type} Wiki Page");
-        $I->wait(1);
-        $I->see("My First Wiki {$type} Public Page!", 'h1');
-
-        $I->seeElement('.fa-globe');
-        $this->toIndex($I);
-        $I->see("First Public {$type} Wiki Page", '.wiki-page-list');
-
-        /**
-         * CREATE PRIVATE PAGE
-         */
-        $I->amGoingTo('Create my private wiki page');
-        $I->click('New page');
-        $I->waitForText('Create new page', 30);
-        $I->fillField('#wikipage-title', "First Private {$type} Wiki Page");
-        $I->fillField('#wikipagerevision-content .humhub-ui-richtext', "# My First Wiki {$type} Private Page!");
-        $I->click('Save');
-
-        $I->waitForElementVisible('.wiki-page-content');
-        $I->see("First Private {$type} Wiki Page");
-        $I->wait(1);
-        $I->see("My First Wiki {$type} Private Page!", 'h1');
-        $I->dontSee("Public", 'h1 .label-info');
-        $this->toIndex($I);
-        $I->see("First Public {$type} Wiki Page", '.wiki-page-list');
-        $I->see("First Private {$type} Wiki Page", '.wiki-page-list');
-    }
-
-    /**
-     * @param AcceptanceTester $I
-     * @throws \Exception
-     */
-    private function toIndex($I)
-    {
-        $I->click('#wiki_index');
-        $I->waitForText('Index', null, '.wiki-content');
-    }
-
-    // Create Category
-    // Add Page
-    // Move Page
-    // Set as homepage
-    // Edit and revert
 }
