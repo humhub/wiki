34309be00c34d8ff1885de73c71bb1a5559f4861
Yuriy Bakhtin <yurybakh@gmail.com>
2022-12-08 15:30:58 +0400

    Redesign 2.0 - Update sidebar styles

--- resources/js/humhub.wiki.CategoryListView.js
+++ resources/js/humhub.wiki.CategoryListView.js
@@ -10,6 +10,11 @@ humhub.module('wiki.CategoryListView', function(module, require, $) {
 
     var DELAY_DRAG_SMALL_DEVICES = 250;
 
+    CategoryListView.prototype.indent = {
+        default: 12,
+        level: 20,
+    };
+
     CategoryListView.prototype.init = function() {
         this.$.find('.fa-caret-down, .fa-caret-right').on('click', function() {
             var $icon = $(this);
@@ -28,46 +33,43 @@ humhub.module('wiki.CategoryListView', function(module, require, $) {
             });
         });
 
-        this.$.sortable({
-            delay: (view.isSmall()) ? DELAY_DRAG_SMALL_DEVICES : null,
+        this.$.find('.wiki-page-list').add(this.$).sortable({
+            delay: view.isSmall() ? DELAY_DRAG_SMALL_DEVICES : null,
             handle: '.drag-icon',
             connectWith: '.wiki-page-list',
             items: '[data-page-id]',
             helper: 'clone',
             placeholder: 'ui-sortable-drop-area',
+            change: $.proxy(this.changePosition, this),
             over: $.proxy(this.overList, this),
-            out: $.proxy(this.outList, this),
-            receive: $.proxy(this.beforeDropItem, this),
-            update: $.proxy(this.dropItem, this)
-        });
-
-        this.$.find('.wiki-page-list').sortable({
-            delay: (view.isSmall()) ? DELAY_DRAG_SMALL_DEVICES : null,
-            handle: '.drag-icon',
-            connectWith: '.wiki-page-list',
-            helper: 'clone',
-            placeholder: 'ui-sortable-drop-area',
-            over: $.proxy(this.overList, this),
-            out: $.proxy(this.outList, this),
+            out: $.proxy(this.clearDroppablePlaceholder, this),
             receive: $.proxy(this.beforeDropItem, this),
             update: $.proxy(this.dropItem, this)
         });
     };
 
+    CategoryListView.prototype.changePosition = function () {
+        var dropArea = $('.ui-sortable-drop-area');
+        var parentIndent = parseInt(dropArea.parent().prev('.page-title').css('padding-left'));
+        dropArea.css('margin-left', (parentIndent - this.indent.default) + 'px');
+    }
+
     CategoryListView.prototype.overList = function (event, ui) {
-        $('.wiki-page-current-droppable').removeClass('wiki-page-current-droppable');
+        this.clearDroppablePlaceholder();
         ui.placeholder.closest('.wiki-page-list').prev('div').addClass('wiki-page-current-droppable');
     }
-    CategoryListView.prototype.outList = function (event, ui) {
+
+    CategoryListView.prototype.clearDroppablePlaceholder = function () {
         $('.wiki-page-current-droppable').removeClass('wiki-page-current-droppable');
     }
 
     CategoryListView.prototype.beforeDropItem = function (event, ui) {
+        var that = this;
         var fixListIndent = function ($item) {
             var $list = $item.closest('.wiki-page-list');
             if ($list.length) {
                 var title = $list.prev('.page-title');
-                var indent = title.length ? parseInt(title.css('padding-left')) + 20 : 12;
+                var indent = title.length ? parseInt(title.css('padding-left')) + that.indent.level : that.indent.default;
                 $item.children('.page-title').css('padding-left', indent + 'px');
                 $item.children('.wiki-page-list').children('li').each(function () {
                     fixListIndent($(this));
@@ -90,7 +92,7 @@ humhub.module('wiki.CategoryListView', function(module, require, $) {
             'ItemDrop[index]': $item.index()
         };
 
-        $('.wiki-page-current-droppable').removeClass('wiki-page-current-droppable');
+        this.clearDroppablePlaceholder();
 
         client.post(this.options.dropUrl, {data: data}).then(function (response) {
             if (!response.success) {
