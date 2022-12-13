3cb79b91ddd57a1ebc354be70dea85927891c7cc
Yuriy Bakhtin <yurybakh@gmail.com>
2022-12-13 11:00:10 +0400

    Redesign 2.0 - Update styles for "+" icon for long titles in sidebar

--- widgets/views/pageListItemTitle.php
+++ widgets/views/pageListItemTitle.php
@@ -1,7 +1,6 @@
 <?php
 
 use humhub\libs\Html;
-use humhub\modules\wiki\helpers\Helper;
 use humhub\widgets\Button;
 use humhub\modules\wiki\helpers\Url;
 
@@ -14,9 +13,10 @@ use humhub\modules\wiki\helpers\Url;
 /* @var $options array */
 ?>
 <?= Html::beginTag('div', $options) ?>
-    <?= Button::asLink()->icon('arrows')->cssClass('wiki-page-control drag-icon')->visible($page && $showDrag) ?>
-    <i class="fa <?= $icon ?>"></i> <?= Html::a(Html::encode($title), $url, ['class' => 'page-title-text']) ?>
-
+    <div>
+        <?= Button::asLink()->icon('arrows')->cssClass('wiki-page-control drag-icon')->visible($page && $showDrag) ?>
+        <i class="fa <?= $icon ?>"></i> <?= Html::a(Html::encode($title), $url, ['class' => 'page-title-text']) ?>
+    </div>
     <?php if ($page && $showAddPage) : ?>
         <?= Button::asLink(null, Url::toWikiCreateForCategory($page))->icon('fa-plus')
             ->cssClass('wiki-page-control tt wiki-category-add')
