dc58905f7bd3eb1e7e6c9b2bbd6fc1beec530a9a
Yuriy Bakhtin <yurybakh@gmail.com>
2023-02-02 18:57:30 +0400

    Fix click on last edited row

--- resources/js/humhub.wiki.bundle.js
+++ resources/js/humhub.wiki.bundle.js
@@ -946,8 +946,11 @@ humhub.module('wiki.ListTable', function(module, require, $) {
     const ListTable = Widget.extend();
 
     ListTable.prototype.init = function() {
-        this.$.find('.table > tbody > tr > td').on('click', function() {
-            const link = $(this).find('a.wiki-page-list-row-title');
+        this.$.find('.table > tbody > tr > td').on('click', function(e) {
+            if ($(e.target).prop('tagName') === 'A') {
+                return;
+            }
+            const link = $(this).find('.wiki-page-list-row-title a');
             if (link.length) {
                 location.href = link.attr('href');
             }
