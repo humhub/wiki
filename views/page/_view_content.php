0311fc4142a567cd17f8f14a07881a6481ba83d0
vidhyasagargm <vidhyasagargsm@gmail.com>
2024-09-10 18:23:56 +0200

    Added numbering to content

--- views/page/_view_content.php
+++ views/page/_view_content.php
@@ -4,6 +4,7 @@ use humhub\libs\Html;
 use humhub\modules\topic\models\Topic;
 use humhub\modules\topic\widgets\TopicLabel;
 use humhub\modules\wiki\widgets\WikiRichText;
+use humhub\modules\wiki\widgets\WikiRichTextNumberingExtension;
 use humhub\widgets\Button;
 use humhub\modules\wiki\helpers\Url;
 
@@ -11,6 +12,39 @@ use humhub\modules\wiki\helpers\Url;
 /* @var $page \humhub\modules\wiki\models\WikiPage */
 /* @var $canEdit bool */
 /* @var $content string */
+// Function to add numbering to headers
+function addNumberingToHeaders($content) {
+    $headerCounters = [0, 0, 0, 0, 0, 0]; // To keep track of header levels
+
+    // Regex to match markdown headers (e.g., #, ##, ###)
+    return preg_replace_callback('/^(#+)\s*(.+)/m', function ($matches) use (&$headerCounters) {
+        $headerLevel = strlen($matches[1]);
+
+        // Reset counters for deeper levels
+        for ($i = $headerLevel; $i < 6; $i++) {
+            $headerCounters[$i] = 0;
+        }
+
+        // Increment the counter for the current header level
+        $headerCounters[$headerLevel - 1]++;
+
+        // Build the numbering string (e.g., 1.2.3)
+        $numbering = '';
+        for ($i = 0; $i < $headerLevel; $i++) {
+            if ($headerCounters[$i] > 0) {
+                $numbering .= $headerCounters[$i] . '.';
+            }
+        }
+
+        // Return the header with the numbering added
+        return $matches[1] . ' ' . trim($numbering, '.') . ' ' . $matches[2];
+    }, $content);
+}
+
+// Apply header numbering to the content before rendering it
+if (!empty($content)) {
+    $content = addNumberingToHeaders($content);
+}
 ?>
 <div class="topic-label-list">
 <?php foreach ($page->content->getTags(Topic::class)->all() as $topic) : ?>
@@ -23,7 +57,7 @@ use humhub\modules\wiki\helpers\Url;
 
 <?php if (!empty($content)) : ?>
     <div class="markdown-render" data-ui-widget="wiki.Page"<?= $canEdit ? ' data-edit-url="' . Url::toWikiEdit($page) . '"' : '' ?> data-ui-init style="display:none">
-        <?= WikiRichText::output($content, ['id' => 'wiki-page-richtext']) ?>
+	<?= WikiRichText::output($content, ['id' => 'wiki-page-richtext']) ?>
     </div>
 <?php else: ?>
     <br>
