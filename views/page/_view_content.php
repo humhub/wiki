f8862059625b3b885a75b4a26fb13668a7ed8d01
vidhyasagargm <85664242+vidhyasagargm@users.noreply.github.com>
2024-09-25 03:29:12 -0700

    Update _view_content.php
    removed additional space and tabspace

--- views/page/_view_content.php
+++ views/page/_view_content.php
@@ -11,7 +11,6 @@ use humhub\modules\wiki\helpers\Url;
 /* @var $page \humhub\modules\wiki\models\WikiPage */
 /* @var $canEdit bool */
 /* @var $content string */
-
 ?>
 
 <!-- CSS styling for numbering of content -->
@@ -35,16 +34,17 @@ use humhub\modules\wiki\helpers\Url;
 
 <div class="topic-label-list">
 <?php foreach ($page->content->getTags(Topic::class)->all() as $topic) : ?>
-  <?= TopicLabel::forTopic($topic) ?>
+   <?= TopicLabel::forTopic($topic) ?>
 <?php endforeach; ?>
 </div>
 
 <h1 class="wiki-page-title"><?= Html::encode($page->title) ?></h1>
+
 <?= $this->render('_view_category_index', ['page' => $page]) ?>
 
 <?php if (!empty($content)) : ?>
     <div class="markdown-render" data-ui-widget="wiki.Page"<?= $canEdit ? ' data-edit-url="' . Url::toWikiEdit($page) . '"' : '' ?> data-ui-init style="display:none">
-	<?= WikiRichText::output($content, ['id' => 'wiki-page-richtext']) ?>
+        <?= WikiRichText::output($content, ['id' => 'wiki-page-richtext']) ?>
     </div>
 <?php else: ?>
     <br>
